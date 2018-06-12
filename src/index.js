2f1cf9604c4037e8b7d1a3ade38cb22f402a9b42
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-12 11:03:58 -0700

    react-intro - Dan Parker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj = {
+    name: 'Dan',
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1000)
+}
+
+function greeting(userObj){
+   // debugger;
+    const element = (<div className='container'>
+    <h1>Hello, {userObj.name}</h1>
+    <h2 className='text-muted'>this is your lucky number: {userObj.luckyNumber}</h2>
+</div>);
+    return element;
+    
+}
+
+
+
+
+// const element = (
+//     <div className='container'>
+//         <h1>Hello, </h1>
+//         <h2 className='text-muted'>this is your lucky number: luckyNumber</h2>
+//     </div>
+// );
