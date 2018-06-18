19d3581f4b0b1e18dd2be63b614f28708501c588
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-18 14:37:52 -0700

    started address book

---
+++ src/components/contact_list.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ContactCard from './contact_card';
+
+class ContactList extends Component{
+    constructor(props){
+        super(props)
+        
+        this.state = {
+    }
+
+    
+
+    }
+
+    render(){
+        return (
+            <h1>Contact List</h1>
+        )
+    }
+}
+
+export default ContactList;
---
+++ src/data/contacts.js
@@ -0,0 +1,24 @@
+export default [
+    
+    {
+        firstName: 'Dan',
+        lastName: 'Parker',
+        phone: '394-493-493',
+        email: 'dan@gmail.com'
+    }, 
+    
+    {
+        firstName: 'Josh',
+        lastName: 'Smith',
+        phone: '394-543-493',
+        email: 'josh@gmail.com'
+    }, 
+    
+    {
+        firstName: 'Abby',
+        lastName: 'Graham',
+        phone: '334-493-493',
+        email: 'Abby@gmail.com'
+    }
+
+];
