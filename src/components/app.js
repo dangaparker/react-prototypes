841c77bffd6890fdea64e44e49ceb470e16dac5f
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-20 20:00:59 -0700

    initial react-routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,24 @@
+import React from 'react';
+import {Route}from 'react-router-dom';
+import Welcome from './welcome';
+import Nav from './nav';
+import Macarons from './our_macarons';
+import Contact from './contact';
+import GiftsParties from './giftsparties';
+
+export default ()=> {
+
+    return(
+    <div className="container">
+    <Nav/>
+    <Route exact path = '/' component = {Welcome}/>
+    <Route path = '/our-macarons' component = {Macarons}/>
+    <Route path = '/contact' component = {Contact}/>
+    <Route path = '/gifts-parties' component = {GiftsParties}/>
+        
+    </div>
+    )
+}
+    
 
 
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default ()=> (
+    <div>
+        <h1>Welcome</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis rem pariatur eveniet commodi! Inventore nostrum hic odit aliquam, soluta eligendi.</p>
+    </div>
+)
