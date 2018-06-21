9bc19ed0265a0a1af69e42f1a51d0efd92ddf0ab
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-20 18:46:59 -0700

    completed basic scrapbook

---
+++ src/components/image_data.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import beach from '../images/beach1.jpg'
+import beach2 from '../images/beach2.jpeg'
+import beach3 from '../images/beach3.jpg'
+import beach4 from '../images/beach4.jpg'
+import beach5 from '../images/beach5.jpg'
+import beach6 from '../images/beach6.jpg'
+import beach7 from '../images/beach7.jpg'
+
+
+export default [
+    {
+        src: beach,
+        style: {left: '5%', transform: 'rotate(-10deg)'}
+    },
+    {
+        src: beach2,
+        style: {left: '40%', transform: 'rotate(6deg)'}
+    },
+    {
+        src: beach3,
+        style: {right: '15%', transform: 'rotate(-9deg)'}
+    },
+    {
+        src: beach4,
+        style: {left: '8%', top: '28%', transform: 'rotate(8deg)'}
+    },
+    {
+        src: beach5,
+        style: {left: '37%', top: '25%', transform: 'rotate(-12deg)'}
+   },
+    {
+        src: beach6,
+        style: {right: '12%', top: '26%', transform: 'rotate(-5deg)'}
+    },
+    {
+        src: beach7,
+        style: {right: '25%', top: '52%', transform: 'rotate(16deg)'}
+    },
+];
