15203c2a933ddb6faababa7cda506f94e8fe7736
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-23 13:31:37 -0700

    redux start

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,7 @@
+import React from 'react';
 
-
+() => {
+    <div className='container'>
+        <h1 className='text-center'>Redux Clock</h1>
+    </div>
+}
---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,8 @@
+const DEFAULT_STATE = {time: null}
+
+export default (state = DEFAULT_STATE, action)=> {
+    switch(action.type){
+        default: 
+            return state;
+    }
+}
